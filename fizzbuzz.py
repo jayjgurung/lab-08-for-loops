c5e0a8f6db2aaa47632e939db06fc075cb8d7cf1
jayjgurung <newuser@Jxxded-MacBook-Pro.local>
2023-10-25 16:55:18 -0400

    +revision average.html, fizzbuzz .html & .py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 for counter in range(1,101):
-    if counter%3 == 0:
+    if (counter%3==0) & (counter%5==0): #question whats ther diff between and and && or &
+        print("FizzBuzz")
+    elif counter%3 == 0:
         print("Fizz")
     elif counter%5 ==0:
         print("Buzz")
